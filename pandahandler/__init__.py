737c1b056257d940b3c4f3a99d7d91e5abd8bf82
Zach Kurtz <zkurtz@protonmail.com>
2024-12-24 07:33:15 -0500

    expose public methods

--- pandahandler/__init__.py
+++ pandahandler/__init__.py
@@ -1,3 +1,7 @@
 from importlib.metadata import version
 
 __version__ = version("pandahandler")
+__all__ = ["Schema", "categorize_non_numerics"]
+
+from pandahandler.schema import Schema as Schema
+from pandahandler.schema import categorize_non_numerics as categorize_non_numerics
